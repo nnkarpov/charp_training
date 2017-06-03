86d8cb437bc801ce1021be362da0cfcaaad59d1d
nnkarpov <nnkarpov@users.noreply.github.com>
2017-06-03 14:50:28 +0300

    Обновлен config_inc.php для проекта mantis

--- mantis-tests/mantis-tests/config_inc.php
+++ mantis-tests/mantis-tests/config_inc.php
@@ -8,3 +8,4 @@ $g_db_password            = '';
 $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = '/8mhRy7zSOlYpBgg/qNZQpnpcaHAcRiAwhPJ/bG0x6w=';
+$g_signup_use_captcha = OFF;
